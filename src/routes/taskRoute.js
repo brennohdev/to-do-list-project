ca748aefe1365caf06a682200983f4dbd555113d
Brenno Henrique <brennohdev@gmail.com>
2025-03-13 22:18:25 -0300

    Implement task management routes and remove unused authentication script

---
+++ src/routes/taskRoute.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+// POST : create task (201)
+// GET: List tasks (200)
+// GET: Get specific task (200)
+// PUT: Update task (200)
+// PATCH: Update task's status (200)
+// DELETE: Delete one task (204)
